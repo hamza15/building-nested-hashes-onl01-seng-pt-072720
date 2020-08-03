b7efbb1536245114566e96bcb18600541a51ab34
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-03 19:34:31 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -39,5 +39,5 @@ def fourth_challenge
 }
 end
 
-epic_tragedy[:montague][:hero][:status] = "dead"
+fourth_challenge.epic_tragedy[:montague][:hero][:status] = "dead"
 epic_tragedy[:capulet][:heroine][:status] = "dead"
