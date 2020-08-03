a599bc106bf3c4b933b2df8537eb2ade1d86b9e1
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-03 19:17:59 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -17,9 +17,7 @@ def third_challenge
         :age : "15",
         :status : "alive"
         },
-      :hero_friends => [{
-        :name
-      }]
+      :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
